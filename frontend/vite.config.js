5fd17c1a2bfb47da271801dedc87d716a282f818
Sahil Kamble <syk2448@gmail.com>
2025-08-12 13:30:50 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:4000", // development
-        target: "https://formbuilder-qpp8.onrender.com/", // prod
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  // server: {
+  //   // proxy: {
+  //   //   "/api": {
+  //   //     // target: "http://localhost:4000", // development
+  //   //     target: "https://formbuilder-qpp8.onrender.com/", // prod
+  //   //     changeOrigin: true,
+  //   //     rewrite: (path) => path.replace(/^\/api/, ""),
+  //   //   },
+  //   },
+  // },
 });
