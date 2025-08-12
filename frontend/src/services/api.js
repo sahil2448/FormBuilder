1979352f91a59aedb4ce0d8e5e62c6c4a078a96f
Sahil Kamble <syk2448@gmail.com>
2025-08-12 13:06:49 +0530

    pushing for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,9 +2,11 @@
 import axios from "axios";
 
 const API_BASE_URL = "http://localhost:4000";
+const PROD = "https://formbuilder-qpp8.onrender.com/";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  // baseURL: API_BASE_URL, // dev
+  baseURL: PROD, // prod
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        // target: "http://localhost:4000", // development
+        target: "https://formbuilder-qpp8.onrender.com/", // prod
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
